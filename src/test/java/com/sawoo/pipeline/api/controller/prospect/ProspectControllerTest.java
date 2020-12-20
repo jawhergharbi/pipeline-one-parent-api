56255156d656d0e268cb6475e53b81e8ac8c3b78
Miguel Maquieira <miguel.maquieira@gmail.com>
2020-12-20 20:13:07 +0100

    ProspectControllerTest testing CompanyValidator

--- src/test/java/com/sawoo/pipeline/api/controller/prospect/ProspectControllerTest.java
+++ src/test/java/com/sawoo/pipeline/api/controller/prospect/ProspectControllerTest.java
@@ -2,6 +2,7 @@ package com.sawoo.pipeline.api.controller.prospect;
 
 import com.sawoo.pipeline.api.controller.ControllerConstants;
 import com.sawoo.pipeline.api.controller.base.BaseControllerTest;
+import com.sawoo.pipeline.api.dto.company.CompanyDTO;
 import com.sawoo.pipeline.api.dto.prospect.ProspectDTO;
 import com.sawoo.pipeline.api.mock.ProspectMockFactory;
 import com.sawoo.pipeline.api.model.DBConstants;
@@ -21,8 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -78,6 +78,60 @@ public class ProspectControllerTest extends BaseControllerTest<ProspectDTO, Pros
 
                 // Validate the returned fields
                 .andExpect(jsonPath("$.messages", hasSize(1)));
+    }
+
+    @Test
+    @DisplayName("POST /api/prospect: company id and company name not informed - Failure")
+    void createWhenCompanyIdAndCompanyNameNotInformedReturnsFailure() throws Exception {
+        String PROSPECT_LAST_NAME = getMockFactory().getFAKER().name().lastName();
+        String PROSPECT_FIRST_NAME = getMockFactory().getFAKER().name().firstName();
+        ProspectDTO postEntity = getMockFactory().newDTO(null, PROSPECT_FIRST_NAME, PROSPECT_LAST_NAME);
+        postEntity.getCompany().setName(null);
+
+        // Execute the POST request
+        mockMvc.perform(post(getResourceURI())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(postEntity)))
+
+                // Validate the response code and the content type
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+
+                // Validate the returned fields
+                .andExpect(jsonPath("$.messages", hasSize(1)))
+                .andExpect(jsonPath("$.messages[0]",
+                        containsString("Field company must include either the [id] field or both [name and url] fields")));
+    }
+
+    @Test
+    @DisplayName("POST /api/prospect: company id informed but company name and url not informed - Success")
+    void createWhenCompanyIdInformedAndCompanyNameAndCompanyUrlNotInformedReturnsSuccess() throws Exception {
+        String PROSPECT_LAST_NAME = getMockFactory().getFAKER().name().lastName();
+        String PROSPECT_FIRST_NAME = getMockFactory().getFAKER().name().firstName();
+        ProspectDTO postEntity = getMockFactory().newDTO(null, PROSPECT_FIRST_NAME, PROSPECT_LAST_NAME);
+        postEntity.setCompany(CompanyDTO.builder().id(getMockFactory().getComponentId()).build());
+
+        String PROSPECT_ID = getMockFactory().getComponentId();
+        ProspectDTO mockedEntity = getMockFactory().newDTO(PROSPECT_ID, postEntity);
+
+        // setup the mocked service
+        doReturn(mockedEntity).when(service).create(ArgumentMatchers.any(ProspectDTO.class));
+
+        // Execute the POST request
+        mockMvc.perform(post(getResourceURI())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(postEntity)))
+
+                // Validate the response code and the content type
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+
+                // Validate the headers
+                .andExpect(header().string(HttpHeaders.LOCATION, getResourceURI() + "/" + PROSPECT_ID))
+
+                // Validate the returned fields
+                .andExpect(jsonPath("$.id", is(PROSPECT_ID)))
+                .andExpect(jsonPath("$.company").exists());
     }
 
     @Test
