957c13539357f24effe1ded331bba6ca27e031c4
Miguel Maquieira <miguel.maquieira@gmail.com>
2020-12-29 18:22:34 +0100

    cleaning up old classes

--- src/main/java/com/sawoo/pipeline/api/service/common/CommonServiceMapper.java
+++ src/main/java/com/sawoo/pipeline/api/service/common/CommonServiceMapper.java
@@ -5,11 +5,7 @@ import com.sawoo.pipeline.api.dto.client.ClientBaseDTO;
 import com.sawoo.pipeline.api.dto.client.ClientBasicDTO;
 import com.sawoo.pipeline.api.dto.interaction.InteractionDTO;
 import com.sawoo.pipeline.api.dto.prospect.LeadBaseDTO;
-import com.sawoo.pipeline.api.dto.prospect.LeadDTOOld;
 import com.sawoo.pipeline.api.dto.prospect.LeadInteractionDTOOld;
-import com.sawoo.pipeline.api.dto.prospect.LeadMainDTOOld;
-import com.sawoo.pipeline.api.dto.user.UserDTOOld;
-import com.sawoo.pipeline.api.model.UserOld;
 import com.sawoo.pipeline.api.model.client.Client;
 import com.sawoo.pipeline.api.model.prospect.LeadInteractionOld;
 import com.sawoo.pipeline.api.model.prospect.LeadOld;
@@ -22,19 +18,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommonServiceMapper {
 
-    private final JMapper<LeadDTOOld, LeadOld> leadDomainToDTOMapper = new JMapper<>(LeadDTOOld.class, LeadOld.class);
     private final JMapper<LeadBaseDTO, LeadOld> leadDomainToDTOBaseMapper = new JMapper<>(LeadBaseDTO.class, LeadOld.class);
-    private final JMapper<LeadMainDTOOld, LeadOld> leadDomainToDTOMainMapper = new JMapper<>(LeadMainDTOOld.class, LeadOld.class);
 
-    private final JMapper<LeadOld, LeadDTOOld> leadDTOToDomainMapper = new JMapper<>(LeadOld.class, LeadDTOOld.class);
     private final JMapper<LeadInteractionDTOOld, LeadInteractionOld> leadInteractionDomainToDTOMapper = new JMapper<>(LeadInteractionDTOOld.class, LeadInteractionOld.class);
     private final JMapper<InteractionDTO, LeadInteractionOld> interactionDomainToDTOMapper = new JMapper<>(InteractionDTO.class, LeadInteractionOld.class);
 
     private final JMapper<ClientBasicDTO, Client> clientDomainToDTOBasicMapper = new JMapper<>(ClientBasicDTO.class, Client.class);
     private final JMapper<Client, ClientBasicDTO> clientDTOToDomainMapper = new JMapper<>(Client.class, ClientBasicDTO.class);
     private final JMapper<ClientBaseDTO, Client> clientDomainToDTOBaseMapper = new JMapper<>(ClientBaseDTO.class, Client.class);
-
-    private final JMapper<UserDTOOld, UserOld> userDomainToDTOMapper = new JMapper<>(UserDTOOld.class, UserOld.class);
-    private final JMapper<UserOld, UserDTOOld> userDTOToDomainMapper = new JMapper<>(UserOld.class, UserDTOOld.class);
 
 }
