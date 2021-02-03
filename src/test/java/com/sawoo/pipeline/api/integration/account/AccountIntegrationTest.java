3a73066bbe40c68b8a1af9984ac56aa9ef5a4c5a
Miguel Maquieira <miguel.maquieira@gmail.com>
2021-02-03 11:50:06 +0100

    testing creating account with user entity

--- src/test/java/com/sawoo/pipeline/api/integration/account/AccountIntegrationTest.java
+++ src/test/java/com/sawoo/pipeline/api/integration/account/AccountIntegrationTest.java
@@ -9,6 +9,8 @@ import com.sawoo.pipeline.api.integration.base.BaseIntegrationTest;
 import com.sawoo.pipeline.api.mock.AccountMockFactory;
 import com.sawoo.pipeline.api.model.DBConstants;
 import com.sawoo.pipeline.api.model.account.Account;
+import com.sawoo.pipeline.api.model.user.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
@@ -20,10 +22,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -61,7 +67,10 @@ public class AccountIntegrationTest extends BaseIntegrationTest<AccountDTO, Acco
     @DisplayName("POST /api/account: create account must also create the user - Success")
     @MongoCleanUp(collectionNames = {DBConstants.ACCOUNT_DOCUMENT, DBConstants.USER_DOCUMENT, DBConstants.COMPANY_DOCUMENT})
     void createWhenEntityNotFoundReturnsSuccess() throws Exception {
+        // Set up the mock entities
         AccountDTO postEntity = getMockFactory().newDTO(null);
+        String EMAIL = postEntity.getEmail();
+        String COMPANY_NAME = postEntity.getCompany().getName();
 
         // Execute the POST request
         getMockMvc().perform(post(getResourceURI())
@@ -73,6 +82,59 @@ public class AccountIntegrationTest extends BaseIntegrationTest<AccountDTO, Acco
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
 
                 // Validate the returned fields
-                .andExpect(jsonPath("$.id").exists());
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.email", is(EMAIL)))
+                .andExpect(jsonPath("$.created").exists());
+
+        // Assertions
+        Query query = Query.query(Criteria.where("email").is(EMAIL));
+        Account account = getMongoTemplate().findOne(query, Account.class);
+        Assertions.assertAll("Assert account entity has been properly stored",
+                () -> Assertions.assertNotNull(
+                        account,
+                        String.format("Account not found with email: [%s]", EMAIL)),
+                () -> Assertions.assertNotNull(
+                        account.getCompany(),
+                        String.format("Company can not be null for account with email: [%s]", EMAIL)),
+                () -> Assertions.assertEquals(
+                        COMPANY_NAME,
+                        account.getCompany().getName(),
+                        String.format("Company name must be [%s]", COMPANY_NAME)),
+                () -> Assertions.assertFalse(
+                        account.getUsers().isEmpty(),
+                        "List of users can not be empty"
+                ));
+    }
+
+    @Test
+    @Order(11)
+    @DisplayName("POST /api/account: create account when user is already created - Failure")
+    @MongoCleanUp(collectionNames = {DBConstants.ACCOUNT_DOCUMENT, DBConstants.USER_DOCUMENT, DBConstants.COMPANY_DOCUMENT})
+    void createWhenEntityNotFoundButUserFoundReturnsFailure() throws Exception {
+        // Set up the mock entities
+        AccountDTO postEntity = getMockFactory().newDTO(null);
+        String EMAIL = postEntity.getEmail();
+        User user = getMockFactory().getUserMockFactory().newEntity(null);
+        user.setFullName(postEntity.getFullName());
+        user.setEmail(EMAIL);
+        getMongoTemplate().save(user);
+
+        // Execute the POST request
+        getMockMvc().perform(post(getResourceURI())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(postEntity)))
+
+                // Validate the response code and the content type
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+
+                // Validate the returned fields
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.message",
+                        stringContainsInOrder("Entity type [user]", "already exits in the system")));
+
+        Query query = Query.query(Criteria.where("email").is(EMAIL));
+        Account account = getMongoTemplate().findOne(query, Account.class);
+        Assertions.assertNull(account, String.format("Account with email: [%s] must be null", EMAIL));
     }
 }
