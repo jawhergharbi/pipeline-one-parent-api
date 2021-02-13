84aadc265adf25c1a7142dfc449e64435fee5e25
Miguel Maquieira <miguel.maquieira@gmail.com>
2021-02-13 19:21:35 +0100

    first version of the sequence with advanced query based on the users

--- src/main/java/com/sawoo/pipeline/api/repository/sequence/SequenceRepository.java
+++ src/main/java/com/sawoo/pipeline/api/repository/sequence/SequenceRepository.java
@@ -1,7 +1,24 @@
 package com.sawoo.pipeline.api.repository.sequence;
 
 import com.sawoo.pipeline.api.model.sequence.Sequence;
+import com.sawoo.pipeline.api.model.sequence.SequenceUserType;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Set;
 
 public interface SequenceRepository extends MongoRepository<Sequence, String> {
+
+    @Query(value = "{ 'users': { $elemMatch: { 'userId': ?0 } }}")
+    List<Sequence> findByUser(String userId);
+
+    @Query(value = "{ 'users': { $elemMatch: { 'userId': {$in: ?0} } } }")
+    List<Sequence> findByUsers(Set<String> userIds);
+
+    @Query(value = "{ 'users': { $elemMatch: { 'userId': ?0, 'type': ?1 } }}")
+    List<Sequence> findByUserIdAndUserType(String userId, SequenceUserType type);
+
+    @Query(value = "{ 'users': { $elemMatch: { 'userId': {$in: ?0}, 'type': ?1 } }}")
+    List<Sequence> findByUserIdsAndUserType(Set<String> userIds, SequenceUserType type);
 }
--- src/test/java/com/sawoo/pipeline/api/repository/sequence/SequenceRepositoryTest.java
+++ src/test/java/com/sawoo/pipeline/api/repository/sequence/SequenceRepositoryTest.java
@@ -2,14 +2,23 @@ package com.sawoo.pipeline.api.repository.sequence;
 
 import com.sawoo.pipeline.api.mock.SequenceMockFactory;
 import com.sawoo.pipeline.api.model.sequence.Sequence;
+import com.sawoo.pipeline.api.model.sequence.SequenceUserType;
 import com.sawoo.pipeline.api.repository.base.BaseRepositoryTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -42,5 +51,105 @@ public class SequenceRepositoryTest extends BaseRepositoryTest<Sequence, Sequenc
     protected Sequence getNewEntity() {
         String SEQUENCE_ID = getMockFactory().getFAKER().internet().uuid();
         return getMockFactory().newEntity(SEQUENCE_ID);
+    }
+
+    @Test
+    @DisplayName("findByUserId: sequences found - Success")
+    void findByUserWhenUserIsPartOfSequencesReturnsSuccess() {
+        String USER_ID = "6027a3436fb12b99f63b0e23";
+        int SEQUENCES_FOUND = 1;
+
+        List<Sequence> sequences = getRepository().findByUser(USER_ID);
+
+        Assertions.assertFalse(sequences.isEmpty(), "List of sequences can not be empty");
+        Assertions.assertEquals(
+                SEQUENCES_FOUND,
+                sequences.size(),
+                String.format("[%d] sequence/s must be found for user with id [%s]", SEQUENCES_FOUND, USER_ID));
+    }
+
+    @Test
+    @DisplayName("findByUser: sequences not found - Success")
+    void findByUserWhenUserIsNotPartOfSequencesReturnsSuccess() {
+        String USER_ID = "wrong_id";
+
+        List<Sequence> sequences = getRepository().findByUser(USER_ID);
+
+        Assertions.assertTrue(sequences.isEmpty(), "List of sequences must be empty");
+    }
+
+    @Test
+    @DisplayName("findByUsers: sequences found - Success")
+    void findByUsersWhenUsersArePartOfSequencesReturnsSuccess() {
+        String USER_ID_1 = "6027a3436fb12b99f63b0e23";
+        String USER_ID_2 = "6027a2ff4542c0de858d2936";
+        int SEQUENCES_FOUND = 3;
+
+        List<Sequence> sequences = getRepository().findByUsers(new HashSet<>(Arrays.asList(USER_ID_1, USER_ID_2)));
+
+        Assertions.assertFalse(sequences.isEmpty(), "List of sequences can not be empty");
+        Assertions.assertEquals(
+                SEQUENCES_FOUND,
+                sequences.size(),
+                String.format("[%d] sequence/s must be found for users with ids [%s]", SEQUENCES_FOUND, Arrays.asList(USER_ID_1, USER_ID_2)));
+    }
+
+    @Test
+    @DisplayName("findByUsers: sequences found and only one userId provided- Success")
+    void findByUsersWhenUserIsPartOfSequencesReturnsSuccess() {
+        String USER_ID = "6027a3436fb12b99f63b0e23";
+        int SEQUENCES_FOUND = 1;
+
+        List<Sequence> sequences = getRepository().findByUsers(new HashSet<>(Collections.singletonList(USER_ID)));
+
+        Assertions.assertFalse(sequences.isEmpty(), "List of sequences can not be empty");
+        Assertions.assertEquals(
+                SEQUENCES_FOUND,
+                sequences.size(),
+                String.format(
+                        "[%d] sequence/s must be found for users with ids [%s]",
+                        SEQUENCES_FOUND,
+                        Collections.singletonList(USER_ID)));
+    }
+
+    @Test
+    @DisplayName("findByUserIdAndUserType: sequences found - Success")
+    void findByUserIdAndUserTypeWhenUserIsPartOfSequencesReturnsSuccess() {
+        String USER_ID = "6027a2ff4542c0de858d2936";
+        int SEQUENCES_FOUND = 2;
+
+        List<Sequence> sequences = getRepository().findByUserIdAndUserType(USER_ID, SequenceUserType.OWNER);
+
+        Assertions.assertFalse(sequences.isEmpty(), "List of sequences can not be empty");
+        Assertions.assertEquals(
+                SEQUENCES_FOUND,
+                sequences.size(),
+                String.format(
+                        "[%d] sequence/s must be found for user with id [%s] and user type [%s]",
+                        SEQUENCES_FOUND,
+                        USER_ID,
+                        SequenceUserType.OWNER));
+    }
+
+    @Test
+    @DisplayName("findByUserIdsAndUserType: sequences found - Success")
+    void findByUserIdsAndUserTypeWhenUserIsPartOfSequencesReturnsSuccess() {
+        String USER_ID = "6027a2ff4542c0de858d2936";
+        int SEQUENCES_FOUND = 1;
+
+        List<Sequence> sequences = getRepository()
+                .findByUserIdsAndUserType(
+                        new HashSet<>(Collections.singletonList(USER_ID)),
+                        SequenceUserType.EDITOR);
+
+        Assertions.assertFalse(sequences.isEmpty(), "List of sequences can not be empty");
+        Assertions.assertEquals(
+                SEQUENCES_FOUND,
+                sequences.size(),
+                String.format(
+                        "[%d] sequence/s must be found for user with id [%s] and user type [%s]",
+                        SEQUENCES_FOUND,
+                        USER_ID,
+                        SequenceUserType.EDITOR));
     }
 }
