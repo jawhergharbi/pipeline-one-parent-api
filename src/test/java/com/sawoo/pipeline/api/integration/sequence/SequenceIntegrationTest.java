9cdc784df4852d7c28392c806d7890d6c793f91e
Miguel Maquieira <miguel.maquieira@gmail.com>
2021-02-14 19:02:42 +0100

    Sequence component Integration test component added

---
+++ src/test/java/com/sawoo/pipeline/api/integration/sequence/SequenceIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.sawoo.pipeline.api.integration.sequence;
+
+import com.sawoo.pipeline.api.controller.ControllerConstants;
+import com.sawoo.pipeline.api.dto.sequence.SequenceDTO;
+import com.sawoo.pipeline.api.integration.MongoDataFile;
+import com.sawoo.pipeline.api.integration.MongoSpringExtension;
+import com.sawoo.pipeline.api.integration.base.BaseIntegrationTest;
+import com.sawoo.pipeline.api.mock.SequenceMockFactory;
+import com.sawoo.pipeline.api.model.DBConstants;
+import com.sawoo.pipeline.api.model.sequence.Sequence;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc(addFilters = false)
+@Tags({@Tag(value = "integration")})
+@Profile(value = {"unit-tests", "unit-tests-embedded"})
+@ExtendWith({SpringExtension.class, MongoSpringExtension.class})
+@MongoDataFile(
+        value = "sequence-integration-test-data.json",
+        classType = Sequence.class,
+        collectionNames = {DBConstants.SEQUENCE_DOCUMENT})
+public class SequenceIntegrationTest extends BaseIntegrationTest<SequenceDTO, Sequence, SequenceMockFactory> {
+
+    private static final String SEQUENCE_INTEGRATION_EXPECTED_RESULTS_FILE_NAME = "sequence-integration-expected-results.json";
+
+    @Autowired
+    public SequenceIntegrationTest(MockMvc mockMvc, MongoTemplate mongoTemplate, SequenceMockFactory mockFactory) {
+        super(mockMvc, mongoTemplate,
+                ControllerConstants.SEQUENCE_CONTROLLER_API_BASE_URI,
+                DBConstants.SEQUENCE_DOCUMENT,
+                SEQUENCE_INTEGRATION_EXPECTED_RESULTS_FILE_NAME,
+                mockFactory);
+    }
+
+    @Override
+    protected Class<Sequence> getClazz() {
+        return Sequence.class;
+    }
+}
