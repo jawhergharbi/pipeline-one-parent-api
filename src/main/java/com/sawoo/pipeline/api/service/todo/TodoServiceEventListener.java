7de44fe2b77a41ec78c40602fb499f572ca39c1e
Miguel Maquieira <miguel.maquieira@gmail.com>
2021-04-04 21:41:46 +0200

    manual todos include source

--- src/main/java/com/sawoo/pipeline/api/service/todo/TodoServiceEventListener.java
+++ src/main/java/com/sawoo/pipeline/api/service/todo/TodoServiceEventListener.java
@@ -1,12 +1,17 @@
 package com.sawoo.pipeline.api.service.todo;
 
 import com.sawoo.pipeline.api.dto.todo.TodoDTO;
+import com.sawoo.pipeline.api.dto.user.UserAuthDetails;
 import com.sawoo.pipeline.api.model.common.Link;
 import com.sawoo.pipeline.api.model.common.LinkType;
 import com.sawoo.pipeline.api.model.todo.Todo;
+import com.sawoo.pipeline.api.model.todo.TodoSource;
+import com.sawoo.pipeline.api.model.todo.TodoSourceType;
 import com.sawoo.pipeline.api.service.base.event.BaseServiceBeforeInsertEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -28,5 +33,23 @@ public class TodoServiceEventListener {
                 entity.getLink().setType(LinkType.PLAIN_LINK);
             }
         }
+
+        // Source. Not informed. It would be a manual task
+        if (entity.getSource() == null) {
+
+            entity.setSource(TodoSource.builder()
+                    .type(TodoSourceType.MANUAL)
+                    .build());
+            UserAuthDetails user = getUserDetails();
+            if (user != null) {
+                entity.getSource().setSourceId(user.getId());
+                entity.getSource().setSourceId(user.getFullName());
+            }
+        }
+    }
+
+    private UserAuthDetails getUserDetails() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return (UserAuthDetails) auth.getPrincipal();
     }
 }
